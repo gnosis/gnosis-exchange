9334ab00f466c3117e309570d09d54000ffc1ad5
Alan Lu <alanlu1023@gmail.com>
2017-02-10 12:01:28 -0600

    Made a useful test

--- test/TestExchange.sol
+++ test/TestExchange.sol
@@ -2,29 +2,19 @@ pragma solidity ^0.4.2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
+import "tokens/StandardToken.sol";
 import "../contracts/Exchange.sol";
 
 contract TestExchange {
+    using Assert for *;
 
-    function testSomething() {
-        uint expected = 1;
-        Assert.equal(1, expected, "What?");
+    function testExchangePairIdentifiersSymmetric() {
+        address toka = address(new StandardToken());
+        address tokb = address(new StandardToken());
+        Exchange exchange = Exchange(DeployedAddresses.Exchange());
+        exchange.calcExchangeIdentifier([toka, tokb])
+            .equal(exchange.calcExchangeIdentifier([tokb, toka]),
+            "Exchange ID not symmetric on tokens");
     }
-
-  /*function testInitialBalanceUsingDeployedContract() {
-    Exchange meta = Exchange(DeployedAddresses.Exchange());
-
-    uint expected = 10000;
-
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 Exchange initially");
-  }
-
-  function testInitialBalanceWithNewExchange() {
-    Exchange meta = new Exchange();
-
-    uint expected = 10000;
-
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 Exchange initially");
-  }*/
 
 }
